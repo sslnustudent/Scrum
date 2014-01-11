ace76efe44980916098252000f5532619951636b
sslnustudent <ss222tb@student.lnu.se>
2014-01-11 01:38:24 +0100

    Day3

--- ScrumProject/ScrumProject/Program.cs
+++ ScrumProject/ScrumProject/Program.cs
@@ -11,7 +11,28 @@ namespace ScrumProject
         static void Main(string[] args)
         {
             List<Member> members = new List<Member>();
-            int id = 1;
+            members.Add(new Member()
+            {
+                Name = "Lars",
+                Surname = "Sturesson",
+                Number = 234354,
+                IdNumber = 1
+            });
+            members.Add(new Member()
+            {
+                Name = "Philip",
+                Surname = "Blad",
+                Number = 234978,
+                IdNumber = 2
+            });
+            members.Add(new Member()
+            {
+                Name = "Göran",
+                Surname = "Kjellsson",
+                Number = 8756456,
+                IdNumber = 3
+            });
+            int id = 4;
             int menuchoice;
             while (true)
             {
@@ -25,10 +46,27 @@ namespace ScrumProject
                         id++;
                         break;
                     case 2:
+                        if (members.Count == 0)
+                        {
+                            Console.WriteLine("Det finns inga registrerade medlemar");
+                        }
+                        else
+                        {
+                            MemberList(members);
+                        }
                         break;
                     case 3:
+                        
                         break;
                     case 4:
+                        if (members.Count == 0)
+                        {
+                            Console.WriteLine("Det finns inga registrerade medlemar");
+                        }
+                        else
+                        {
+                            RemoveMember(members);
+                        }
                         break;
                     case 5:
                         break;
@@ -113,6 +151,90 @@ namespace ScrumProject
                 Number = number,
                 IdNumber = id
             });
+        }
+
+        static void MemberList(List<Member> members)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("============================================");
+            Console.WriteLine("           Lista över medlemar              ");
+            Console.WriteLine("============================================\n");
+            Console.ResetColor();
+            for (int i = 0; i < members.Count; i++ )
+            {
+                Console.WriteLine(members[i].Name + " " + members[i].Surname);
+            }
+            ContinueOnKeyPressed();
+        }
+
+        static void RemoveMember(List<Member> members)
+        {
+            ConsoleKeyInfo key;
+            int removeid;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("============================================");
+            Console.WriteLine("           Lista över medlemar              ");
+            Console.WriteLine("============================================\n");
+            Console.ResetColor();
+            for (int i = 0; i < members.Count; i++)
+            {
+                Console.WriteLine(members[i].Name + " " + members[i].Surname + " ID: " + (i+1));
+            }
+            Console.Write("\nSkriv in IDet på medlemen du vill ta\nbort eller skriv 0 för att avbryta ");
+            removeid = int.Parse(Console.ReadLine());
+            if (removeid < 0 || removeid > (members.Count + 1))
+            { 
+            }
+            else if (removeid == 0)
+            {
+            }
+            else
+            {
+                while (true)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Är du säker på att du vill ta bort " + members[removeid - 1].Name + " " + members[removeid - 1].Surname);
+                    Console.ResetColor();
+
+                    key = Console.ReadKey();
+
+                    if (key.Key == ConsoleKey.J)
+                    {
+                        members.RemoveAt(removeid - 1);
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.DarkGreen;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("============================================");
+                        Console.WriteLine("          Medlemen har tagits bort          ");
+                        Console.WriteLine("============================================");
+                        Console.ResetColor();
+                        ContinueOnKeyPressed();
+                        return;
+                    }
+                    else if (key.Key == ConsoleKey.N)
+                    {
+
+                        return;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("============================================");
+                        Console.WriteLine("  ***Ogilltig tangent tryck J eller N ***   ");
+                        Console.WriteLine("============================================");
+                        Console.ResetColor();
+                    }
+
+                }
+                
+            }
         }
     }
 }
